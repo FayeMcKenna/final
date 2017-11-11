99f0bba42a81504fce5fcb0006ac22c78340ddec
JonAbebe <31824906+JonAbebe@users.noreply.github.com>
2017-11-11 13:56:27 -0500

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -89,33 +89,38 @@ def matching(dic):
     return final_dic
 
 new_columns = matching(new_sheets)
-
+#Since plate 11 is empty in Gender, Age and Hospital, new columns were created under those names and made
+#empty for ease of the graph_production function
+new_columns['Plate11']['Gender'] = ''
+new_columns['Plate11']['Age'] = ''
+new_columns['Plate11']['Hospital ']= ''
 
 def graph_production(dic):
-    # ITERATE OVER plates to have all of them!
     '''Creates a graph with y being the value and x being the dilution'''
-    plots = dic['Plate 1'] #Considering only plate 1 in the dictionary for ease of testing
-    group = plots.groupby('patient_id') #Groups by the patient ID
-    lst = ['PSMalpha2','ABA','PSMalpha3'] #For testing purposes, to see accuracy of plotting 3 different columns
-    plotting_columns = plots.loc[:,'PSMalpha2':'Tetanus Toxoid'] #Saves all columns between to plotting_columns to plot
-    cols_dropped_NaN = plotting_columns.dropna(axis=1,how='all') #Gets rid of all columns containing NaN
-    col_to_plot = cols_dropped_NaN.columns #Extracts all the column names and saves it to a variable
-    for i,j in group: #For loops through the grouped dataframe
-        group2 = j.groupby('replicate') #Groups by the replicate (V1,V2,V3..)
-        for l in lst: #Goes through the list of columns you want to print, lst is for testing, col_to_plot is the final list
-            fig, ax = plt.subplots()
-            for key,values in group2: #Goes through the keys (V1,V2,V3) and the corresponding values
-                values.plot('dilution', l, label = key,ax = ax, logy = True)
-            plt.ylabel('Intensity',fontsize= 14,fontweight = 'bold')
-            plt.xlabel('Dilution',fontsize = 14,fontweight = 'bold')
+    # plots = dic['Plate 1'] #Considering only plate 1 in the dictionary for ease of testing
+    for plates,vals in dic.items(): #Goes through all the plates in the created dictionary
+        group = vals.groupby('patient_id') #Groups by the patient ID
+        lst = ['PSMalpha2','ABA'] #For testing purposes, to see accuracy of plotting 3 different columns
+        plotting_columns = vals.loc[:,'PSMalpha2':'Tetanus Toxoid'] #Saves all columns between to plotting_columns to plot
+        cols_dropped_NaN = plotting_columns.dropna(axis=1,how='all') #Gets rid of all columns containing NaN
+        col_to_plot = cols_dropped_NaN.columns #Extracts all the column names and saves it to a variable
+        for i,j in group: #For loops through the grouped dataframe
+            group2 = j.groupby('replicate') #Groups by the replicate (V1,V2,V3..)
+            for l in col_to_plot: #Goes through the list of columns you want to print, lst is for testing, col_to_plot is the final list
+                fig, ax = plt.subplots()
+                for key,values in group2: #Goes through the keys (V1,V2,V3) and the corresponding values
+                    values.plot('dilution', l, label = key,ax = ax, logy = True)
+                plt.ylabel('Intensity',fontsize= 14,fontweight = 'bold') #Creates the ylabel
+                plt.xlabel('Dilution',fontsize = 14,fontweight = 'bold') #Creates the xlabel
+                if i != 'Standard': #If the sample is not a standard the title becomes the gender, age hospital and column, formatted
+                    title = '{}({} {} {}) {}'.format(i,j.iloc[0]['Gender'],j.iloc[0]['Age'], j.iloc[0]['Hospital '],l )
+                else: #else it just becomes the Standard and the column
+                    title = '{} {}'.format('Standard',l)
+                fig.suptitle(title,fontsize= 14,fontweight = 'bold')
+    plt.show()
 
-            if i != 'Standard': #If the sample is not a standard the title becomes the gender, age and hospital, formatted
-                title = '{}({} {} {}) {}'.format(i,j.iloc[0]['Gender'],j.iloc[0]['Age'], j.iloc[0]['Hospital '],l )
-            else: #else it just becomes the Standard
-                title = 'Standard'
-            fig.suptitle(title,fontsize= 14,fontweight = 'bold')
-            plt.show()
-            
-            
-graph_production(new_columns)
+
+
+
+# graph_production(new_columns)
     
